d3e219e9a307067e65d87af50b4963cf303b600d
aldhanekaa <aldhanekadev@gmail.com>
2021-12-02 05:05:56 +0700

    loading page on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,56 @@ import Layout from "components/Layout/index";
 import { getCookie } from "cookies-next";
 import getReservation from "utils/getReservation";
 import { NextSeo } from "next-seo";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Container, CircularProgress, Box } from "@mui/material";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [loaded, setLoaded] = useState(false);
+
+  async function p() {
+    const id = getCookie("id");
+    console.log(id);
+    // @ts-ignore
+    const reservation = await getReservation(id);
+    if (reservation.item) {
+      router.push("/live");
+    }
+
+    setLoaded(true);
+  }
+
+  useEffect(() => {
+    p();
+  }, []);
+
+  if (!loaded) {
+    return (
+      <>
+        <Head>
+          <NextSeo
+            title="TechnoNatura Art Exhibition Live Event | Login"
+            description="Tonton Acara Art Exhibition yang Diselenggrakan TechnoNatura melalui link ini!"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Container
+          sx={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <CircularProgress /> <h3 style={{ marginLeft: 10 }}>Loading..</h3>
+          </Box>
+        </Container>
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
