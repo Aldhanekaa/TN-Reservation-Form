05087b79bbc201d3f8b7d81c1c5647396dd47e76
aldhanekaa <aldhanekadev@gmail.com>
2021-12-02 08:36:28 +0700

    reloading auto when event starts

--- components/main/mainWrapper.tsx
+++ components/main/mainWrapper.tsx
@@ -1,8 +1,21 @@
 import MainSection from "components/main/main";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function MainWrapper() {
   const [currentDuration, setCurrentDuration] = useState(0);
+  const router = useRouter();
+  const liveEventStarts = new Date(
+    "Dec 2 2021 11:30:00 GMT+0700 (Western Indonesia Time)"
+  ).getTime();
+
+  useEffect(() => {
+    setInterval(() => {
+      if (Date.now() >= liveEventStarts) {
+        router.reload();
+      }
+    }, 1000);
+  }, []);
 
   return (
     <MainSection
